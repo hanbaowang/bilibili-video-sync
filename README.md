c4dd642dc01d38498178ab00c875a4c34e00191c
Mingfei Ding <xiesansi@gmail.com>
2020-01-12 18:21:02 +0800

    demo and popup style

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bilibili-video-sync
 A chrome extension for bilibili video syncing between friends 哔哩哔哩 (゜-゜)つロ 干杯~
+
+## player APIs
+
+[flv.js api](https://github.com/bilibili/flv.js/blob/master/docs/api.md#interface-player-abstract)
---
+++ popup.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const input = document.getElementById('ramdon')
+const generate = document.getElementById('generate')
+const matching = document.getElementById('matching')
+
+
+let changeColor = document.getElementById('changeColor');
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
