efe7b54e4f13f970104f328b3a80eaccada690d3
Mingfei Ding <xiesansi@gmail.com>
2020-01-20 22:58:04 +0800

    add notify

--- src/inject.js
+++ src/inject.js
@@ -1,6 +1,7 @@
 import { DeepstreamClient } from '@deepstream/client'
 
 import { Player } from "./player";
+import { bvsNotify } from "./notify";
 class App {
     constructor() {
         this.playerFocusOn = false;
@@ -10,15 +11,18 @@ class App {
         this.id = '';
         this.player = new Player();
         this.record = {};
-
+        this.lastHeartbeat = Date.now();
+        this.connected = false;
+        this.reconnection = 0;
+        this.reconnectInterval = null;
     }
 
     start() {
         this._getStorage()._connect();
         if (this.state === 'hosted') {
-            this._setController();
+            this._setControllers();
         } else {
-            this._setSubscriber();
+            this._setSubscribers();
         }
     }
 
@@ -28,6 +32,18 @@ class App {
         this.id = localStorage['id'];
         this.state = localStorage['state'];
         return this;
+    }
+
+    _heartbeatSubscriber() {
+        let interval = setInterval(() => {
+            if (Date.now() - this.lastHeartbeat > 30 * 1000) {
+                bvsNotify('Lost connection with the host');
+                clearInterval(interval);
+            }
+        }, 5 * 1000)
+        this.record.subscribe('heartbeat', (heartbeat) => {
+            this.lastHeartbeat = heartbeat;
+        })
     }
 
     _setStateSubscriber() {
@@ -50,11 +66,34 @@ class App {
         })
     }
 
+    _welcomeSubscriber() {
+        this.record.subscribe('welcome', (time) => {
+            this.connected = true;
+            if (time - this.lastHeartbeat > 0) {
+                this.lastHeartbeat = time;
+            }
+        })
+    }
+
     _newcomerSubscriber() {
         this.record.subscribe('newcomer', () => {
             this._progressController();
             this._stateController();
+            this._welcomeNewcomer();
+            bvsNotify('Newcomer joined');
         })
+    }
+
+    _welcomeNewcomer() {
+        this.record.set('welcome', Date.now())
+    }
+
+    _heartbeatController() {
+        setInterval(() => {
+            this.record.set('heartbeat', Date.now(), err => {
+                console.log('heartbeat err', err);
+            })
+        }, 5 * 1000)
     }
 
     _stateController(time) {
@@ -64,7 +103,9 @@ class App {
                 this._stateController(500);
                 return
             }
-            this.record.set('state', state);
+            this.record.set('state', state, err => {
+                console.log('test', err)
+            });
             console.log('state has set to', state);
         }, time || 0);
     }
@@ -79,6 +120,20 @@ class App {
 
     _newcomerController() {
         this.record.set('newcomer', 1);
+        this.reconnectInterval = setInterval(() => {
+            if (this.connected === true) {
+                bvsNotify("host founded, enjoy your journey");
+                clearInterval(this.reconnectInterval);
+                return 
+            }
+            if (this.connected === false && this.reconnection < 3) {
+                this.reconnection++;
+                bvsNotify(`we have tried ${this.reconnection} times but no host founded`)
+            } else {
+                bvsNotify(`no host founded, please contact to the host`);
+                clearInterval(this.reconnectInterval);
+            }
+        }, 5 * 1000)
     }
 
     _shortcutController(e) {
@@ -110,11 +165,14 @@ class App {
         client.login();
         this.record = client.record.getRecord(this.id);
         console.log('connected');
+        bvsNotify('connected');
         return this;
     }
 
-    _setController() {
+    _setControllers() {
         this._newcomerSubscriber();
+
+        this._heartbeatController();
         
         document.querySelector('.bilibili-player-video-btn.bilibili-player-video-btn-start')
             .addEventListener('click', this._stateController.bind(this), false);
@@ -131,7 +189,8 @@ class App {
         document.addEventListener('click', this._updatePlayerFoucsOn.bind(this), false);
     }
 
-    _setSubscriber() {
+    _setSubscribers() {
+        this._heartbeatSubscriber();
         this._setStateSubscriber();
         this._setProgressSubscriber();
         this._newcomerController();
